110e2246258f01d4a6959520e4997ca518f225a0
satoshi2 <Chinkirov@the-dma.ru>
2019-12-07 17:55:54 +0300

    feature/ end combine fucntions

--- CombineFunctions.js
+++ CombineFunctions.js
@@ -1,1 +1,11 @@
-module.exports = (middleware) => (ctx, next) => 
+module.exports = (middleware) => (ctx, next) => function dispatch(i){
+    let fn = middleware[i]
+    if (i === middleware.length){
+        fn = next
+    }
+
+    return fn ? 
+        Promise.resolve(fn(ctx, () => dispatch(i + 1 ))) :
+        Promise.resolve()
+
+}
